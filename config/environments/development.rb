40d158fa71a1b1eed850b10fd3fdbfbba6e99c63
hisaju <hisaju@gmail.com>
2024-09-02 17:02:32 +0900

    設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.log_level = :debug
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
